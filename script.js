8da67bd8de23bef3b731ac4fa5ffe0cb4ba676c2
vikas1509 <119025906+vikas1509@users.noreply.github.com>
2023-07-21 16:44:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function fibonacci(num) {
 // your code here
 for (let i = 1; i <= num; i++) {
-    console.log(n1);
+   
     nextTerm = n1 + n2;
     n1 = n2;
     n2 = nextTerm;
 }
+	return n1;
 }
 
 module.exports = fibonacci;
