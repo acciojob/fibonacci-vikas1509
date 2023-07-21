a1997c4458d0148fbeca0f37436904ab415a975d
vikas1509 <119025906+vikas1509@users.noreply.github.com>
2023-07-21 15:51:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
 // your code here
+	var i =0;
+	var j=1;
+	var ans =0;
+	for(const k=1;k<=num;i++){
+		ans=i+k;
+		i=k;
+	}
+	return ans;
 }
 
 module.exports = fibonacci;
